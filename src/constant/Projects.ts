f94a246b736a3eae8f203fdb2f9e4078c39e7790
Hamza Alsherif <87245938+0xHamzaDev@users.noreply.github.com>
2024-09-13 21:21:18 +0300

    update project description

--- src/constant/Projects.ts
+++ src/constant/Projects.ts
@@ -9,12 +9,12 @@ export const projects = [
         id: 2,
         title: 'Banking Application',
         description: "A secure and feature-rich mobile banking application, inspired by popular apps like urpay. The app offers a range of financial services including account management, money transfers, bill payments, and transaction history. It also integrates with third-party services for enhanced features like currency conversion, investment tracking, and digital wallets. Emphasizing security, the app uses biometric authentication and end-to-end encryption to ensure user data is protected. The UI/UX design focuses on providing a smooth and intuitive user experience.",
-        technologies: ['React Native', 'TypeScript', 'Node', 'Express', 'MongoDB'],
+        technologies: ['React Native', 'TypeScript', 'Node'],
     },
     {
         id: 3,
         title: 'DZRT Availability Notifications',
-        description: "A web scraping application developed to monitor and track the availability of products on e-commerce websites. The application sends real-time notifications via email or SMS when a tracked product is back in stock or its status is updated. The system can handle multiple websites simultaneously and includes features such as customizable tracking options, notification preferences, and a user-friendly interface for managing tracked items.",
-        technologies: ['TypeScript', 'Node', 'Express'],
+        description: "A web scraping application developed to monitor and track the availability of products on e-commerce websites. The application sends real-time notifications via telegram when a tracked product is back in stock or its status is updated. The system can handle multiple websites simultaneously and includes features such as customizable tracking options, notification preferences, and a user-friendly interface for managing tracked items.",
+        technologies: ['TypeScript', 'Node'],
     }
 ];
